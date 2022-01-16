d20974fcab05871c3822cd8f8769f5af73b0274a
장준영 <doppelgoer1@naver.com>
2022-01-16 23:23:05 +0900

    svelte-vlog

---
+++ svelte-vlog/src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  //MyName 컴포넌트를 import 해 오자.
+  import MyName from './Component/MyName.svelte';
+</script>
+
+<main>
+  <!-- MyName 컴포넌트에 name이라는 props를 전달  -->
+  <h1>Hello <MyName name="JoY" />!</h1>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
