aed53d9873f48084cfce22cae34da248d656dcf1
장준영 <doppelgoer1@naver.com>
2022-01-21 23:03:13 +0900

    push

--- svelte-vlog/src/App.svelte
+++ svelte-vlog/src/App.svelte
@@ -1,39 +1,30 @@
 <script>
-  import MyName from './Component/MyName.svelte';
-  let name = 'JoY';
-  function changeName() {
-    if (name === 'JoY') {
-      name = 'DoP';
-    } else {
-      name = 'JoY';
-    }
-  }
+  // Contact 와 Home 컴포넌트를 임포트해준다.
+  import Contact from './Component/Contact/Contact.svelte';
+  import Home from './Component/Home/Home.svelte';
+  //페이지 라우팅을 위해서 svelte-routing을 임포트해준다.
+  import {Router, Link, Route} from 'svelte-routing';
 </script>
 
 <main>
-  <h1>Hello <MyName {name} />!</h1>
-  <!-- 버튼을 만들어 봅시다 -->
-  <button on:click={changeName}>바꿔보자!</button>
+  <!-- 아직 url은 뭔지 잘 모르겠다 이후에 추가요망. -->
+  <Router url="">
+    <nav>
+      <!-- 네비게이션을 만들고 -->
+      <!-- Link to에 path를 적어넣는다. -->
+      <Link to="/">Home</Link>
+      <Link to="Contact1">Contact1</Link>
+      <Link to="Contact2">Contact2</Link>
+    </nav>
+
+    <div>
+      <!-- 위에서 적은 Link to에 부합하는 path를 적고
+      해당 path에 보여줄 컴포넌트를 넣는다 -->
+      <Route path="/"><Home /></Route>
+      <!-- 컴포넌트를 라우팅 해주는 방법은 아래 두가지가 있다
+      둘다 똑같습니다! -->
+      <Route path="/Contact1" component={Contact} />
+      <Route path="/Contact2"><Contact /></Route>
+    </div>
+  </Router>
 </main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
