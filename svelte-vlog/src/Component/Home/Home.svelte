279433342083f49e3c861e88d98ab6692ad130b6
장준영 <doppelgoer1@naver.com>
2022-01-28 23:06:05 +0900

    tistory state reactive

--- svelte-vlog/src/Component/Home/Home.svelte
+++ svelte-vlog/src/Component/Home/Home.svelte
@@ -1,1 +1,54 @@
+<script>
+  //number 변수 선언
+  let number = 0;
+  let number2 = 0;
+  $: sum = number + number2;
+  //number가 1씩 더해지는 함수 선언
+  function plusNumber() {
+    number++;
+  }
+  //number가 1씩 빼지는 함수 선언
+  function minusNumber() {
+    number--;
+  }
+  function plusNumber2() {
+    number2++;
+  }
+  //number가 1씩 빼지는 함수 선언
+  function minusNumber2() {
+    number2--;
+  }
+
+  $: if (sum === 10) {
+    alert('합은 10입니다.');
+  }
+  // $: alert(`합111은 ${number}입니다.`);
+</script>
+
 <h1>Home</h1>
+<!-- number를 화면에 보여준다. -->
+<div class="numbers-container">
+  <div class="number-container">
+    <h2>number : {number}</h2>
+
+    <button on:click={plusNumber}>Plus</button>
+    <button on:click={minusNumber}>Minus</button>
+  </div>
+  <div class="number-container">
+    <h2>number2 : {number2}</h2>
+    <button on:click={plusNumber2}>Plus</button>
+    <button on:click={minusNumber2}>Minus</button>
+  </div>
+  <div class="number-container">
+    <h2>sum : {sum}</h2>
+  </div>
+</div>
+
+<style>
+  .numbers-container {
+    display: flex;
+  }
+  .number-container {
+    margin: 0 20px 0 20px;
+  }
+</style>
